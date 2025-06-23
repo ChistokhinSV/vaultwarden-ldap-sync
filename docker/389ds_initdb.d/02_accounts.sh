2c2a45a3bf1ff53a8799556daa81bc3884cec425
Sergei Chistokhin <mail4headless@gmail.com>
2025-06-23 09:07:02 +0200

    fixed demo setup with disabled user

--- docker/389ds_initdb.d/02_accounts.sh
+++ docker/389ds_initdb.d/02_accounts.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ## Now add some entries
-echo "Adding Users user, user2, user3"
+
+echo "Adding Group vaultwarden-users"
+ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
+dn: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
+cn: vaultwarden-users
+objectclass: groupOfNames
+EOF
+
+echo "Adding Users user, user2, user3, user4"
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
 dn: uid=user,cn=users,cn=accounts,${DS_SUFFIX_NAME}
 uid: user
@@ -10,7 +18,9 @@ objectClass: inetorgperson
 objectClass: inetuser
 sn: User
 cn: User
+mail: user@domain.local
 userPassword: {CLEARTEXT}vaultwarden
+memberOf: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
 EOF
 
 echo "Adding User user2"
@@ -22,7 +32,9 @@ objectClass: inetorgperson
 objectClass: inetuser
 sn: User2
 cn: User2
+mail: user2@domain.local
 userPassword: {CLEARTEXT}vaultwarden
+memberOf: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
 EOF
 
 echo "Adding User user3"
@@ -34,22 +46,22 @@ objectClass: inetorgperson
 objectClass: inetuser
 sn: User3
 cn: User3
+mail: user3@domain.local
 userPassword: {CLEARTEXT}vaultwarden
 EOF
 
-echo "Adding Group vaultwarden-users"
+
+echo "Adding User user4"
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
-cn: vaultwarden-users
-objectclass: groupOfNames
-
-EOF
-
-echo "Adding users user, user2 to group vaultwarden-users"
-ldapmodify -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
-changetype: modify
-add: member
-member: uid=user,cn=users,cn=accounts,${DS_SUFFIX_NAME}
-member: uid=user2,cn=users,cn=accounts,${DS_SUFFIX_NAME}
+dn: uid=user4,cn=users,cn=accounts,${DS_SUFFIX_NAME}
+uid: user4
+givenName: User4
+objectClass: inetorgperson
+objectClass: inetuser
+sn: User4
+cn: User4
+mail: user4@domain.local
+userPassword: {CLEARTEXT}vaultwarden
+nsAccountLock: TRUE
+memberOf: cn=vaultwarden-users,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
 EOF
